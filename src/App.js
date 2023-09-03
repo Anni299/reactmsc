3ea7f951cb03943a978958eff8e68f89c8f433f6
Anandita Chaudhary <121678604+Anni299@users.noreply.github.com>
2023-09-03 20:16:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
+import React from 'react';
 import './App.css';
+import FAQs from './FAQs';
+import Header from './Header';
+import Review from './Review';
+import Footer from './Footer';
+import Modal from './Modal';
 
 function App() {
   return (
     <div className="App">
-Hello
+      <Modal />
+      <Header />
+      <FAQs />
+      <Review />
+      <Footer />
     </div>
   );
 }
